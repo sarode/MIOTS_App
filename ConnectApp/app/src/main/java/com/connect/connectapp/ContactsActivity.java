ff3a362bf2e310ceca1c1a5eba2bef941d31723d
Shilpa Sarode <Shilpa Sarode>
2016-02-23 22:27:56 -0500

    Imported contacts

--- ConnectApp/app/src/main/java/com/connect/connectapp/ContactsActivity.java
+++ ConnectApp/app/src/main/java/com/connect/connectapp/ContactsActivity.java
@@ -1,16 +1,83 @@
 package com.connect.connectapp;
 
+import android.Manifest;
+import android.app.ListActivity;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
 
-public class ContactsActivity extends AppCompatActivity {
+public class ContactsActivity extends ListActivity {
+
+    @Override
+    public long getSelectedItemId() {
+        return super.getSelectedItemId();
+    }
+
+    @Override
+    public int getSelectedItemPosition() {
+        return super.getSelectedItemPosition();
+    }
+
+    ListView lv;
+    Cursor cursor1;
+    public static final int MY_PERMISSIONS_REQUEST_READ_CONTACTS = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.READ_CONTACTS) != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_CONTACTS)) {
+                // show an explanation
+            } else {
+                // No explanation needed
+
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_CONTACTS},
+                        MY_PERMISSIONS_REQUEST_READ_CONTACTS);
+            }
+        } else {
+            this.populateContacts();
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+
+            case MY_PERMISSIONS_REQUEST_READ_CONTACTS: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    this.populateContacts();
+                }
+            }
+        }
+    }
+
+    private void populateContacts() {
+        cursor1 = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null, null, null, null);
+        startManagingCursor(cursor1);
+        String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone._ID};
+
+        int[] to = {android.R.id.text1, android.R.id.text2};
+
+        SimpleCursorAdapter listadapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, cursor1, from, to);
+
+        setListAdapter(listadapter);
+
+        lv = getListView();
+        lv.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
     //** Called when the user clicks the Continue button */
